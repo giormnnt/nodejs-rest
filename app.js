d01ad125d39589fc0b5ff02ad9ae36a420f28f20
k1yeon <gio13remanente@gmail.com>
2021-04-20 06:07:14 +0800

    sending request and response

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+// app.use(bodyParser.urlencoded()) // * x-www-form-urlencoded <form method="POST">
+app.use(express.json()); // * application/json
+
+app.use((req, res, next) => {
+  // * set all urls
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // * allows specifig origin to access content and data
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+
+  // * can send requests that holds extra authorization data
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use('/feed', feedRoutes);
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,16 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+  // * res.json allows to return a response with json data, right headers and many more.
+  res
+    .status(200)
+    .json({ posts: [{ title: 'First Post', content: 'Hello World!' }] });
+};
+
+exports.createPost = (req, res, next) => {
+  const { title, content } = req.body;
+  // * creates post in db
+  // * 200 vs 201; 200 - only success; 201 - resource created successfully.
+  res.status(201).json({
+    message: 'Post created successful!',
+    post: { id: new Date().toISOString(), title, content },
+  });
+};
