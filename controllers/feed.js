c87f00f64304d6efd38be91f5642b33a098721ad
k1yeon <gio13remanente@gmail.com>
2021-04-21 22:17:15 +0800

    server side validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   // * res.json allows to return a response with json data, right headers and many more.
   res.status(200).json({
@@ -17,6 +19,14 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // * 422 validation failed status code
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect',
+      errors: errors.array(),
+    });
+  }
   const { title, content } = req.body;
   // * creates post in db
   // * 200 vs 201; 200 - only success; 201 - resource created successfully.
