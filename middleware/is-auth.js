319738886c7d153aa9401b0f92a9047e594b70a0
k1yeon <gio13remanente@gmail.com>
2021-04-26 06:39:11 +0800

    using and validating jwt token

---
+++ middleware/is-auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const error = require('../util/error');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) error.error401(token, 'Not authenticated.');
+  const token = authHeader.replace('Bearer ', '');
+  let decodedToken;
+  try {
+    // * decodes and verifies token
+    decodedToken = jwt.verify(token, 'thisismysecret');
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+  error.error401(decodedToken, 'Not authenticated.');
+  req.userId = decodedToken.userId;
+  next();
+};
