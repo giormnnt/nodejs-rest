63d64104175d1817d3224e9eb3e8d1f996108769
k1yeon <gio13remanente@gmail.com>
2021-04-27 22:16:56 +0800

    deleting post across clients

--- controllers/feed.js
+++ controllers/feed.js
@@ -133,6 +133,7 @@ exports.deletePost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.pull(postId);
     await user.save();
+    io.getIO().emit('posts', { action: 'delete', post: postId });
     res.status(200).json({ message: 'Deleted Post!' });
   } catch (err) {
     error.error500(err, next);
