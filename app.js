d8702d9257fb3445aae17bdeaebe8a8da73bc8b2
k1yeon <gio13remanente@gmail.com>
2021-05-03 12:09:36 +0800

    added port

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use((error, req, res, next) => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    const server = app.listen(8080);
+    const server = app.listen(process.env.PORT || 8080);
     const io = require('./socket').init(server);
     io.on('connection', socket => {
       console.log('Client connected');
